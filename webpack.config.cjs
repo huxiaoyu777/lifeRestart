ec1110a268c646d3c8e1919745bc55654030abe9
huxiaoyu <498797574@qq.com>
2021-09-09 10:46:51 +0800

    修改端口号

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   entry: './src/index.js',
   devtool: 'eval-cheap-module-source-map',
   devServer: {
+    port: 80,
     static: [
       {
         directory: path.join(__dirname, 'data'),
